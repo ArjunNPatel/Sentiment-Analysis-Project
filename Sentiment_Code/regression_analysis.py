33c170f9af7952471c793303940a69cff1c3830c
ArjunNPatel <133174951+ArjunNPatel@users.noreply.github.com>
2023-07-19 18:13:11 -0400

    Create regression_analysis.py

---
+++ Sentiment_Code/regression_analysis.py
@@ -0,0 +1,16 @@
+import numpy as np
+from  sklearn.linear_model import LinearRegression
+def LinearModel(x,y, xlabel = "S_wa", ylabel = "R_st"):
+    x = np.array(x).reshape(-1,1)
+    y = np.array(y)
+    model = LinearRegression()
+
+    model.fit(x,y)
+    Text = f"r squared is {model.score(x,y)}. Formula is {ylabel} = {model.coef_[0]}*{xlabel} + {model.intercept_}"
+    info = {
+        "Text": Text,
+        "R2": model.score(x,y),
+        "Coef": model.coef_,
+        "Intercept": model.intercept_
+    }
+    return info
