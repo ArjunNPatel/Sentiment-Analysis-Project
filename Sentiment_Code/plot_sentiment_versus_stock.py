d0dc8c1c6a1206fd934030c78e9cf0ab17995bf9
ArjunNPatel <133174951+ArjunNPatel@users.noreply.github.com>
2023-07-19 19:57:07 -0400

    Update plot_sentiment_versus_stock.py

--- Sentiment_Code/plot_sentiment_versus_stock.py
+++ Sentiment_Code/plot_sentiment_versus_stock.py
@@ -4,7 +4,6 @@ Original is the adj close stock value on a day closest to the filing of the 10k.
 New is the adj close stock value on a day closest to {6months, 1 year, 18months, 2 years} after the filing of the 10k.
 Soon we will add regression to this.
 """
-
 import yfinance as yf
 from datetime import datetime
 from datetime import timedelta
@@ -13,8 +12,16 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+from regression_analysis import LinearModel
 # taking input as the date
-def stock_history_correlation(file_name, ticker, forward):
+def stock_history_correlation(
+    file_name, 
+    ticker, 
+    forward, 
+    corpus_type,
+    xlabel = "S_wa", 
+    ylabel = "R_st",
+    ):
     def bad_date(Date):
         if forward == "2yr" and Begindate + timedelta(days = int(2*365)) > datetime.now():
             return True
@@ -23,6 +30,8 @@ def stock_history_correlation(file_name, ticker, forward):
         elif forward == "1yr" and Begindate + timedelta(days = int(365)) > datetime.now():
             return True
         elif forward == "6mo" and Begindate + timedelta(days = int(365/2)) > datetime.now():
+            return True
+        elif forward == "3mo" and Begindate + timedelta(days = int(365/4)) > datetime.now():
             return True
         return False
     history = yf.download(ticker, interval="1d")
@@ -33,12 +42,16 @@ def stock_history_correlation(file_name, ticker, forward):
     with open(file_name) as readfile:
         csv_reader = csv.DictReader(readfile,delimiter= ",")
         for row in csv_reader:
-            Begindate = datetime.strptime(row["Date of 10k Filing"][0:10], "%Y-%m-%d")
+            date_header = "Date of 10k Filing"
+            if corpus_type == "Earnings Call":
+                date_header = "Date of Earnings Call"
+            Begindate = datetime.strptime(row[date_header][0:10], "%Y-%m-%d")
             if(bad_date(Begindate)):
                 continue
             future_from_date.append( {
                     "Date":Begindate,
                     "Date Stock":adj_close(Begindate, history),
+                    "Date + 3mo Stock": adj_close(Begindate + timedelta(days = int(365/4)), history),
                     "Date + 6mo Stock": adj_close(Begindate + timedelta(days= int(365/2)), history),
                     "Date + 1yr Stock": adj_close(Begindate + timedelta(days= int(365)), history),
                     "Date + 18mo Stock": adj_close(Begindate + timedelta(days= int(3*365/2)), history),
@@ -49,20 +62,28 @@ def stock_history_correlation(file_name, ticker, forward):
 
     x = np.array(x)
     y = []
-    #change here to either 6mo, 1yr, 18mo, or 2yr
     for date in future_from_date:
-        
         y.append( (date[f"Date + {forward} Stock"] -  date["Date Stock"]) / date["Date Stock"]  )
     y = np.array(y)
     print(x)
     print(y)
-    plt.plot(x,y,'o')
-    plt.xlabel("Compound Sentiment Score of the 10k")
-    #plt.xticks(ticks = x, labels = list(map(lambda v: str(v)[0:4], x)), rotation = 45)
-    plt.ylabel(f"Percent Growth of the Stock over {forward} after filing the 10k")
-    
 
-    plt.savefig(ticker + f" {forward} vs Compound Sentiment.png", bbox_inches = "tight")
+
+    plt.plot(x,y,'o')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    info = LinearModel(x,y)
+    """" info = {
+        "Text": Text,
+        "R2": model.score(x,y),
+        "Coef": model.coef_,
+        "Intercept": model.intercept_
+    }"""
+    line_input = np.linspace(min(x), max(x))
+    plt.plot(line_input, info["Coef"][0]*line_input + info["Intercept"], label = info["Text"], c = 'red' )
+    plt.legend(loc='best')
+    plt.title(f' Compound Sentiment Score on {ticker}_{corpus_type} vs {forward} Return')
+    plt.savefig(f"{ticker}_{corpus_type}_{forward} Compound Sentiment.jpeg")
     
 
 
@@ -78,4 +99,4 @@ def adj_close(date, history):
             lo = mid+1
     return history.iloc[lo]["Adj Close"] 
 if __name__ == "__main__":
-    stock_history_correlation("/home/arjunnipatel/resultsMSFT.csv", "MSFT", "1yr")
+    stock_history_correlation("resultsAAPL.csv", "AAPL", "1yr","10K")
